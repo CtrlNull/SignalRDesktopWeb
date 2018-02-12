3862a791c5a24fbbd997753255e0d16c281e2852
RaphaelFonseca <raphael@fonseca.tech>
2018-02-12 08:36:29 -0500

    init RalphFonz

---
+++ BASFConnector/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BASFConnector
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            string url = @"http://localhost:8080/";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine(string.Format("Server running at {0}", url));
+                Console.ReadLine();
+            }
+
+
+        }
+        public class TestHub
+        {
+            
+        }
+    }
+}
---
+++ BASFConnector/TestHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin.Cors;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BASFConnector
+{
+    [Microsoft.AspNet.SignalR.Hubs.HubName("TestHub")]
+    public class TestHub
+    {
+        
+    }
+}
